53d2f0edf87b78cb0559e4c3a4bafc22329d26b7
Thomas <thomas.baerentsen@gmail.com>
2019-11-08 11:55:01 +0100

    Added ViewRestaurantsByType view

--- Restaurant/Views.cs
+++ Restaurant/Views.cs
@@ -30,5 +30,36 @@ namespace Restaurant
             }
         }
 
+        public void ViewRestaurantsByType(restaurantsContext context, string type)
+        {
+            List<Restaurant> resList = context.Restaurant.Where(p => p.Type.Equals(type)).ToList();
+
+            foreach (var restaurant in resList)
+            {
+                Console.WriteLine($"Name: {restaurant.NameRes}");
+
+                List<Review> reviewList = restaurant.Review.ToList();
+
+                int totalRating = 0;
+                foreach (var review in reviewList)
+                {
+                    totalRating += review.Stars;
+                }
+
+                int avgRating = totalRating / reviewList.Count();
+
+                Console.WriteLine($"Average rating: {avgRating}");
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (reviewList[i] != null)
+                    {
+                        Console.WriteLine(reviewList[i].ReviewText);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
     }
 }
