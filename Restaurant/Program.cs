e3c7e11547237d2fe8cd1d63215ce1415959df02
Thomas <thomas.baerentsen@gmail.com>
2019-11-12 16:06:50 +0100

    Added menu for user input

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices.ComTypes;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Restaurant
 {
@@ -8,10 +9,57 @@ namespace Restaurant
         static void Main(string[] args)
         {
             restaurantsContext db = new restaurantsContext();
-            
-            Views.ViewMenu(db, "hejvej");
-            Views.ViewRestaurantsByType(db, "Fiskerestaurant");
-            Views.ViewDishReviews(db, "hejvej");
+
+            while (true)
+            {
+                Console.WriteLine("Welcome to the restaurants database!");
+                Console.WriteLine("Please enter a command:");
+                Console.WriteLine("Seed database: s");
+                Console.WriteLine("View list of restaurants: 1");
+                Console.WriteLine("View menu of restaurant: 2");
+                Console.WriteLine("View restaurants by type: 3");
+                Console.WriteLine("View dish reviews of restaurant: 4");
+
+                ConsoleKeyInfo pressed = Console.ReadKey();
+                Console.WriteLine("");
+                switch (pressed.Key)
+                {
+                    case ConsoleKey.S:
+                        Console.WriteLine("Seeding database...");
+                        break;
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Not implemented, don't judge me.");
+                        break;
+                    case ConsoleKey.D2:
+                        Views.ViewMenu(db, ReqAddr());
+                        break;
+                    case ConsoleKey.D3:
+                        Views.ViewRestaurantsByType(db, ReqType());
+                        break;
+                    case ConsoleKey.D4:
+                        Views.ViewDishReviews(db, ReqAddr());
+                        break;
+                    default:
+                        break;
+
+                }
+
+                Console.WriteLine("--------------------------------------------------------");
+                Console.WriteLine("");
+            }
+
+        }
+
+        public static string ReqAddr()
+        {
+            Console.WriteLine("Please enter restaurant address:");
+            return Console.ReadLine();
+        }
+
+        public static string ReqType()
+        {
+            Console.WriteLine("Please enter restaurant type:");
+            return Console.ReadLine();
         }
     }
 }
